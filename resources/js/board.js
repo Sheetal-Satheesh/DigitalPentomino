e4e021ee11b155e782758e5328da76c01f7bb55e
JanNB97 <j.bingemann@gmx.de>
2020-12-09 15:00:02 +0100

    board.js: exchanged JavaScript classes through object constructor

--- resources/js/board.js
+++ resources/js/board.js
@@ -1,36 +1,34 @@
-class Board {
-    /**
-     * @desc:
-     *      It will create a default board
-     *
-     * @param:
-     *       size: {X,Y}
-     * @param:
-     *       color: default color;
-     *       shape: square, rectangle, christmas tree
-     */
-    constructor(size) {
-        this.size = size;
+/**
+ * @desc:
+ *      It will create a default board
+ *
+ * @param:
+ *       size: {X,Y}
+ * @param:
+ *       color: default color;
+ *       shape: square, rectangle, christmas tree
+ */
+function Board(size) {
+    this.size = size;
 
-        this.array = [];
-        for (let i = 0; i < this.size[0]; i++) {
-            let col = [];
-            for (let j = 0; j < this.size[1]; j++) {
-                col.push('X');
-            }
-
-            this.array.push(col);
+    this.array = [];
+    for (let i = 0; i < this.size[0]; i++) {
+        let col = [];
+        for (let j = 0; j < this.size[1]; j++) {
+            col.push('X');
         }
+
+        this.array.push(col);
     }
+}
 
-    display() {
-        document.write("Board:<br>");
-        for (let i = 0; i < this.size[0]; i++) {
-            document.write("|");
-            for (let j = 0; j < this.size[1]; j++) {
-                document.write(this.array[i][j]);
-            }
-            document.write("|<br>");
+Board.prototype.display = function() {
+    document.write("Board:<br>");
+    for (let i = 0; i < this.size[0]; i++) {
+        document.write("|");
+        for (let j = 0; j < this.size[1]; j++) {
+            document.write(this.array[i][j]);
         }
+        document.write("|<br>");
     }
 }
