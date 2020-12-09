3556b38095e4b50d82f60072adb1b50d72b30c59
mmenne1 <marcel.menne@gmx.de>
2020-12-09 22:29:49 +0100

    Converted classes to fucntional notation

--- resources/js/game.js
+++ resources/js/game.js
@@ -1,21 +1,53 @@
-class Game{
-    constructor(){
-        this.play=undefined;
-    }
-    start();
-    stop();
+/**
+ * @desc:
+ *      Creates a new Game, keeping track of scores and timers
+ *
+ * @param:
+ *       none
+ *
+ */
 
-    startTimer();
-    timeCalculate();
-    stopTimer();
-
-    score();
-    getScore();
-
-    save();
-    resume();
+function Game(){
+    this.play=undefined;
+    this.gameScore=0;
+    this.time="";
     
-    getTimes();
+    this.start = function(){
+        console.log("Start game.");
+    }
 
+    this.startTimer = function(){
+        console.log("Start timer.");
+    }
+    
+    this.timeCalculate = function(){
+        console.log("Calculate time.");
+    }
+
+    this.stopTimer = function(){
+        console.log("Stop timer.");
+    }
+
+    this.score = function(){
+        console.log("Add Score.");
+    }
+
+    this.getScore = function(){
+        return this.gameScore;
+    }
+
+    this.save = function(){
+        console.log("Start game.");
+    }
+
+    this.resume = function(){
+        console.log("Resume game.");
+    }
+
+    this.getTime = function(){
+        return this.time;
+    }
 
 }
+
+
