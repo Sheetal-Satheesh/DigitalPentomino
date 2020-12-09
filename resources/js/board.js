4b63f7a68a76674e82eeb6022af87fcf7c6984b5
JanNB97 <j.bingemann@gmx.de>
2020-12-09 16:15:53 +0100

    board.js: added placePentomino method

--- resources/js/board.js
+++ resources/js/board.js
@@ -3,7 +3,7 @@
  *      It will create a default board
  *
  * @param:
- *       size: {X,Y}
+ *       size: {width,height}
  * @param:
  *       color: default color;
  *       shape: square, rectangle, christmas tree
@@ -12,10 +12,12 @@ function Board(size) {
     this.size = size;
 
     this.array = [];
-    for (let i = 0; i < this.size[0]; i++) {
+    let width = size[0];
+    let height = size[1];
+    for (let i = 0; i < width; i++) {
         let col = [];
-        for (let j = 0; j < this.size[1]; j++) {
-            col.push('X');
+        for (let j = 0; j < height; j++) {
+            col.push('O');
         }
 
         this.array.push(col);
@@ -24,11 +26,25 @@ function Board(size) {
 
 Board.prototype.display = function() {
     document.write("Board:<br>");
-    for (let i = 0; i < this.size[0]; i++) {
+    for (let y = 0; y < this.size[1]; y++) {
         document.write("|");
-        for (let j = 0; j < this.size[1]; j++) {
-            document.write(this.array[i][j]);
+        for (let x = 0; x < this.size[0]; x++) {
+            document.write(this.array[x][y]);
         }
         document.write("|<br>");
     }
+}
+
+Board.prototype.placePentomino = function(x, y, pentomino) {
+    // TODO - save pentomino in list
+
+    for (i = 0; i < pentomino.height; i++) {
+        for (j = 0; j < pentomino.width; j++) {
+            if (pentomino.occupied_cells.charAt(i * pentomino.width + j) === '1') {
+                this.array[j][i] = pentomino.name;
+            }
+        }
+    }
+
+    // TODO - return true or false whether occupied or not
 }
