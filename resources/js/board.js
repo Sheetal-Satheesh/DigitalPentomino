93c8e3d42090a663293c1abbf932088fea7b768d
JanNB97 <j.bingemann@gmx.de>
2020-12-10 11:29:05 +0100

    board.js: added documentation and error messages

--- resources/js/board.js
+++ resources/js/board.js
@@ -1,12 +1,9 @@
+const EMPTY_CELL = 'O';
+
 /**
- * @desc:
- *      It will create a default board
- *
- * @param:
- *       size: {width,height}
- * @param:
- *       color: default color;
- *       shape: square, rectangle, christmas tree
+ * It will create a default board of the defined size.
+ * @constructor
+ * @param size {Array} [width,height]
  */
 function Board(size) {
     this.size = size;
@@ -20,13 +17,16 @@ function Board(size) {
     for (let i = 0; i < width; i++) {
         let col = [];
         for (let j = 0; j < height; j++) {
-            col.push('O');
+            col.push(EMPTY_CELL);
         }
 
         this.array.push(col);
     }
 }
 
+/**
+ * Prints board on html document for debugging purposes.
+ */
 Board.prototype.display = function() {
     document.write("Board:<br>");
     for (let y = 0; y < this.size[1]; y++) {
@@ -38,6 +38,13 @@ Board.prototype.display = function() {
     }
 }
 
+/**
+ * Places a new or already existing pentomino piece on the board
+ * @param x new x position
+ * @param y new y position
+ * @param pentomino the piece that should be placed
+ * @returns {string|null} returns null if collision occurred or else a string of the occupied cells
+ */
 Board.prototype.placePentomino = function(x, y, pentomino) {
     if (this.isCollides(x, y, pentomino)) {
         return null;
@@ -77,10 +84,48 @@ Board.prototype.removePentomino = function(x, y, pentomino) {
     // TODO
 }
 
+/**
+ * Get cached position of pentomino inside the board
+ * @param pentomino
+ * @throws {Error} if pentomino is not placed on the board
+ * @returns {Array} [x,y]
+ */
 Board.prototype.getPosition = function(pentomino) {
     if (!this.pentominoPositions.hasOwnProperty(pentomino.name)) {
-        throw "No pentomino: " + pentomino.name + " placed on the board";
+        throw new Error("No pentomino: " + pentomino.name + " placed on the board");
     } else {
         return this.pentominoPositions[pentomino.name];
+    }
+}
+
+/**
+ * Get pentomino object by name
+ * @param name
+ * @throws {Error} if no pentomino with this name is placed on the board
+ * @returns pentomino
+ */
+Board.prototype.getPentominoByName = function (name) {
+    for (let i = 0; i < this.pentominos.length; i++) {
+        let pentomino = this.pentominos[i];
+        if (pentomino.name === name) {
+            return pentomino;
+        }
+    }
+    throw new Error("No pentomino with name: " + name + " on the board");
+}
+
+/**
+ * Get pentomino at the specified position
+ * @param x
+ * @param y
+ * @throws {Error} if there is no pentomino at this position of the board
+ * @returns {pentomino}
+ */
+Board.prototype.getPentominoAtPosition = function(x, y) {
+    if (this.array[x][y] === EMPTY_CELL) {
+        throw new Error("No pentomino at position (" + x + ", " + y + ")");
+    } else {
+        let name = this.array[x][y];
+        return this.getPentominoByName(name);
     }
 }
