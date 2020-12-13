ebe6f2e3ffd1886d885d712be4c155ff928d5785
JanNB97 <j.bingemann@gmx.de>
2020-12-13 10:29:53 +0100

    basic structure of game-controller.js

--- resources/js/game-controller.js
+++ resources/js/game-controller.js
@@ -1,3 +1,17 @@
 function GameController() {
 
 }
+
+// --- --- --- Operations --- --- ---
+
+
+// --- --- --- Edit Game --- --- ---
+GameController.prototype._placePiece = function () {
+
+}
+
+GameController.prototype._removePiece = function () {
+
+}
+
+// --- --- --- Get Information about the Game
