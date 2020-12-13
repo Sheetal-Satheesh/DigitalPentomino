1d5a143eb6dfdc925f2a22fbc5a31912e960bfaa
JanNB97 <j.bingemann@gmx.de>
2020-12-13 11:28:12 +0100

    game-controller.js: added methods to access informations about the game

--- resources/js/game-controller.js
+++ resources/js/game-controller.js
@@ -1,5 +1,5 @@
 function GameController() {
-
+    this._board = null;
 }
 
 // --- --- --- Operations --- --- ---
@@ -10,4 +10,24 @@ GameController.prototype.setGame = function (board) {
     this._board = board;
 }
 
-// --- --- --- Get Information about the Game --- --- ---
+// --- --- --- Get Information About The Game For Loading --- --- ---
+GameController.prototype.getBoardSize = function () {
+    if (this._board === null) throw new Error("Game is not set");
+    return this._board.size;
+}
+
+GameController.prototype.getPentominos = function () {
+    if (this._board === null) throw new Error("Game is not set");
+    return this._board.getPentominos();
+}
+
+GameController.prototype.getPositionOfPentomino = function (pentomino) {
+    if (this._board === null) throw new Error("Game is not set");
+    return this._board.getPosition(pentomino);
+}
+
+// --- --- --- Get Information About The Game For User Interaction --- --- ---
+GameController.prototype.getPentominoAtPosition = function (x, y) {
+    if (this._board === null) throw new Error("Game is not set");
+    return this._board.getPentominoAtPosition(x, y);
+}
