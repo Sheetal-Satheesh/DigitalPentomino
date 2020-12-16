ebfdad9dcfa6c3aeeba69391a325ca1482654e9f
JanNB97 <j.bingemann@gmx.de>
2020-12-16 08:47:36 +0100

    Converted game-controller constructor to class declaration

--- resources/js/game-controller.js
+++ resources/js/game-controller.js
@@ -1,60 +1,62 @@
-function GameController() {
-    this._board = null;
-}
+class GameController {
+    constructor() {
+        this._board = null;
+    }
 
-// --- --- --- Pentomino Operations --- --- ---
-GameController.prototype.movePentominoToPosition = function (pentomino, x, y) {
-    return this._board.placePentomino(pentomino, x, y);
-}
+    // --- --- --- Pentomino Operations --- --- ---
+    movePentominoToPosition(pentomino, x, y) {
+        return this._board.placePentomino(pentomino, x, y);
+    }
 
-GameController.prototype.rotatePentominoAntiClkWise = function (pentomino) {
-    return this._board.rotatePentominoAntiClkWise(pentomino);
-}
+    rotatePentominoAntiClkWise(pentomino) {
+        return this._board.rotatePentominoAntiClkWise(pentomino);
+    }
 
-GameController.prototype.rotatePentominoClkWise = function (pentomino) {
-    return this._board.rotatePentominoClkWise(pentomino);
-}
+    rotatePentominoClkWise(pentomino) {
+        return this._board.rotatePentominoClkWise(pentomino);
+    }
 
-GameController.prototype.mirrorPentominoH = function (pentomino) {
-    return this._board.mirrorPentominoH(pentomino);
-}
+    mirrorPentominoH(pentomino) {
+        return this._board.mirrorPentominoH(pentomino);
+    }
 
-GameController.prototype.mirrorPentominoV = function (pentomino) {
-    return this._board.mirrorPentominoV(pentomino);
-}
+    mirrorPentominoV(pentomino) {
+        return this._board.mirrorPentominoV(pentomino);
+    }
 
-// --- --- --- Set Game --- --- ---
-GameController.prototype.setGame = function (board) {
-    this._board = board;
-}
+    // --- --- --- Set Game --- --- ---
+    setGame(board) {
+        this._board = board;
+    }
 
-// --- --- --- Debugging --- --- ---
-GameController.prototype.display = function () {
-    this._board.display();
-}
+    // --- --- --- Debugging --- --- ---
+    display() {
+        this._board.display();
+    }
 
-GameController.prototype.writeToDocument = function () {
-    this._board.writeToDocument();
-}
+    writeToDocument() {
+        this._board.writeToDocument();
+    }
 
-// --- --- --- Get Information About The Game For Loading --- --- ---
-GameController.prototype.getBoardSize = function () {
-    if (this._board === null) throw new Error("Game is not set");
-    return this._board.size;
-}
+    // --- --- --- Get Information About The Game For Loading --- --- ---
+    getBoardSize() {
+        if (this._board === null) throw new Error("Game is not set");
+        return this._board.size;
+    }
 
-GameController.prototype.getPentominos = function () {
-    if (this._board === null) throw new Error("Game is not set");
-    return this._board.getPentominos();
-}
+    getPentominos() {
+        if (this._board === null) throw new Error("Game is not set");
+        return this._board.getPentominos();
+    }
 
-GameController.prototype.getPositionOfPentomino = function (pentomino) {
-    if (this._board === null) throw new Error("Game is not set");
-    return this._board.getPosition(pentomino);
-}
+    getPositionOfPentomino(pentomino) {
+        if (this._board === null) throw new Error("Game is not set");
+        return this._board.getPosition(pentomino);
+    }
 
-// --- --- --- Get Information About The Game For User Interaction --- --- ---
-GameController.prototype.getPentominoAtPosition = function (x, y) {
-    if (this._board === null) throw new Error("Game is not set");
-    return this._board.getPentominoAtPosition(x, y);
+    // --- --- --- Get Information About The Game For User Interaction --- --- ---
+    getPentominoAtPosition(x, y) {
+        if (this._board === null) throw new Error("Game is not set");
+        return this._board.getPentominoAtPosition(x, y);
+    }
 }
