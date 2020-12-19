c8e8d3f600fbc4aa78df5ab39212f4a2f38dd3d4
Hari Kizhedath <hari@mail.uni-paderborn.de>
2020-12-19 20:11:38 +0100

    Changed pentomino to 5x5 representation

--- resources/js/pentomino.js
+++ resources/js/pentomino.js
@@ -1,4 +1,6 @@
 "use strict";
+const I_COLS = 5;
+const I_ROWS = 5;
 
 
 class Pentomino {
@@ -10,63 +12,63 @@ class Pentomino {
         switch (sPentominoType) {
             case 'F':
                 this.iRows = 3;
-                this.iCols = 3;
-                this.sRepr = '011110010';
+                this.iCols = 3; 
+                this.sRepr = '0000000110011000010000000';
                 break;
             case 'L':
                 this.iRows = 4;
                 this.iCols = 2;
-                this.sRepr = '10101011';
+                this.sRepr = '0010000100001000011000000';
                 break;
             case 'N':
                 this.iRows = 4;
-                this.iCols = 2;
-                this.sRepr = '01011110';
+                this,iCols = 2;
+                this.sRepr = '0010000100011000100000000';
                 break;
             case 'P':
                 this.iRows = 3;
                 this.iCols = 2;
-                this.sRepr = '111110';
+                this.sRepr = '0000001100011000100000000';
                 break;
             case 'Y':
                 this.iRows = 2;
                 this.iCols = 4;
-                this.sRepr = '00101111';
+                this.sRepr = '0000000000001001111000000';
                 break;
             case 'T':
                 this.iRows = 3;
                 this.iCols = 3;
-                this.sRepr = '111010010';
+                this.sRepr = '0000001110001000010000000';
                 break;
             case 'U':
                 this.iRows = 2;
                 this.iCols = 3;
-                this.sRepr = '101111';
+                this.sRepr = '0000000000010100111000000';
                 break;
             case 'V':
                 this.iRows = 3;
                 this.iCols = 3;
-                this.sRepr = '100100111';
+                this.sRepr = '0000001000010000111000000';
                 break;
             case 'W':
                 this.iRows = 3;
                 this.iCols = 3;
-                this.sRepr = '100110011';
+                this.sRepr = '0000001000011000011000000';
                 break;
             case 'Z':
                 this.iRows = 3;
                 this.iCols = 3;
-                this.sRepr = '110010011';
+                this.sRepr = '0000001100001000011000000';
                 break;
             case 'I':
-                this.iRows = 1;
-                this.iCols = 5;
-                this.sRepr = '11111';
+                this.iRows = 5;
+                this.iCols = 1;
+                this.sRepr = '0010000100001000010000100';
                 break;
             case 'X':
                 this.iRows = 3;
                 this.iCols = 3;
-                this.sRepr = '010111010';
+                this.sRepr = '0000000100011100010000000';
                 break;
             default:
                 throw 'Unexpected Pentomino Type';
@@ -74,43 +76,35 @@ class Pentomino {
     }
 
     rotateClkWise() {
-        let iCols = this.iCols;
-        let iRows = this.iRows;
         let aNewRepr = [];
 
-        for (let i = iCols; i > 0; --i) {
-            for (let j = iRows; j > 0; --j) {
-                aNewRepr.push(this.sRepr[iCols * j - i]);
+        for (let i = I_COLS; i > 0; --i) {
+            for (let j = I_ROWS; j > 0; --j) {
+                aNewRepr.push(this.sRepr[I_COLS * j - i]);
             }
         }
 
         this.sRepr = aNewRepr.join("");
-        this.iRows = iCols;
-        this.iCols = iRows;
     }
 
     rotateAntiClkWise() {
-        let iCols = this.iCols;
-        let iRows = this.iRows;
         let aNewRepr = [];
 
-        for (let i = 1; i <= iCols; ++i) {
-            for (let j = 1; j <= iRows; ++j) {
-                aNewRepr.push(this.sRepr[iCols * j - i]);
+        for (let i = 1; i <= I_COLS; ++i) {
+            for (let j = 1; j <= I_ROWS; ++j) {
+                aNewRepr.push(this.sRepr[I_COLS * j - i]);
             }
         }
 
         this.sRepr = aNewRepr.join("");
-        this.iRows = iCols;
-        this.iCols = iRows;
     }
 
     mirrorH() {
         let aNewRepr = [];
 
-        for (let i = this.iRows - 1; i >= 0; --i) {
-            for (let j = 0; j < this.iCols; ++j) {
-                aNewRepr.push(this.sRepr[this.iCols * i + j]);
+        for (let i = I_ROWS - 1; i >= 0; --i) {
+            for (let j = 0; j < I_COLS; ++j) {
+                aNewRepr.push(this.sRepr[I_COLS * i + j]);
             }
         }
 
@@ -120,9 +114,9 @@ class Pentomino {
     mirrorV() {
         let aNewRepr = [];
 
-        for (let i = 1; i <= this.iRows; ++i) {
-            for (let j = 1; j <= this.iCols; ++j) {
-                aNewRepr.push(this.sRepr[this.iCols * i - j]);
+        for (let i = 1; i <= I_ROWS; ++i) {
+            for (let j = 1; j <= I_ROWS; ++j) {
+                aNewRepr.push(this.sRepr[I_ROWS * i - j]);
             }
         }
 
@@ -131,12 +125,22 @@ class Pentomino {
 
     getMatrixRepresentation() {
         let aPentomino = Array(5).fill(0).map(() => new Array(5).fill(0));
-        for (let i = 0; i < this.iRows; ++i) {
-            for (let j = 0; j < this.iCols; ++j) {
-                aPentomino[i][j] = parseInt(this.sRepr[i * this.iCols + j]);
+        for (let i = 0; i < I_ROWS; ++i) {
+            for (let j = 0; j < I_COLS; ++j) {
+                aPentomino[i][j] = parseInt(this.sRepr[i * I_COLS + j]);
             }
         }
 
         return aPentomino;
+    }
+
+    display() {
+        let aTemp = '';
+        for(let i = 0; i < I_ROWS; ++i){
+            aTemp = '|';
+            for(let j = 0; j < I_COLS; ++j)
+                aTemp = aTemp.concat(   this.sRepr[i*I_COLS+j]);
+            console.log(aTemp.concat('|'));
+        }
     }
 }
