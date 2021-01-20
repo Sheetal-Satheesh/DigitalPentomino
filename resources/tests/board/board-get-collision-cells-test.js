bb3f7a6a4881b0d95764e2cd22ae6adabf1f63d6
JanNB97 <j.bingemann@gmx.de>
2021-01-20 16:01:19 +0100

    Test collision in board instead of game

--- resources/tests/board/board-get-collision-cells-test.js
+++ resources/tests/board/board-get-collision-cells-test.js
@@ -2,26 +2,25 @@ let assert = require('chai').assert;
 
 let Pentomino = require('../../js/pentomino.js');
 let Board = require('../../js/board.js');
-let Game = require('../../js/game.js');
 
-let game = new Game(new Board(7, 7));
+let board = new Board(7, 7);
 
 let T = new Pentomino('T');
 let I = new Pentomino('I');
 let X = new Pentomino('X');
 let F = new Pentomino('F');
 
-game.placePentomino(T, 1, 3);
-game.placePentomino(I, 3, 1);
-game.placePentomino(X, -1, -1);
-game.placePentomino(F, 4, 2);
+board.placePentomino(T, 1, 3);
+board.placePentomino(I, 3, 1);
+board.placePentomino(F, 4, 2);
 
-game.display();
+board.display();
 
-describe('Game.getCollisionCells()', function() {
+describe('Board.getCollisionCells()', function() {
 
     it('should return a list of all colliding cells', function() {
-        assert.strictEqual(game.getCollisionCells(), [
+        assert.strictEqual(board.getCollisionCells().length, 1);
+        assert.strictEqual(board.getCollisionCells(), [
             {
                 "cell": [1, 1],
                 "pentominos": ['X', 'Y', 'T']
