22cf347dbb1ab3c48f7c5da7eb1b61d0910f63b2
JanNB97 <j.bingemann@gmx.de>
2021-01-27 20:26:28 +0100

    Add HintAI files

---
+++ resources/js/hint-ai.js
@@ -0,0 +1,94 @@
+if(typeof require != 'undefined') {
+    Pentomino = require('./pentomino.js');
+    Board = require('./board.js');
+    Game = require('./game.js');
+}
+
+class HintAI {
+    getMockSolutions() {
+        let solutions = [];
+
+        let X = new Pentomino('X');
+        let T = new Pentomino('T');
+        let L = new Pentomino('L');
+        let U = new Pentomino('U');
+        let N = new Pentomino('N');
+        let F = new Pentomino('F');
+        let I = new Pentomino('I');
+        let P = new Pentomino('P');
+        let Z = new Pentomino('Z');
+        let V = new Pentomino('V');
+        let W = new Pentomino('W');
+        let Y = new Pentomino('Y');
+
+        let game1 = new Game(new Board([0, 0], [5, 7]));
+
+        game1.placePentomino(L, 3, 0);
+
+        game1.placePentomino(F, 1, 1);
+        game1.mirrorPentominoV(F);
+
+        game1.placePentomino(I, 0, 4);
+        game1.rotatePentominoClkWise(I);
+
+        game1.placePentomino(N, 1, 3);
+        game1.rotatePentominoClkWise(N);
+        game1.mirrorPentominoH(N);
+
+        game1.placePentomino(P, 4, 2);
+        game1.rotatePentominoClkWise(P);
+
+        game1.placePentomino(U, 3, 4);
+        game1.rotatePentominoClkWise(U);
+
+        game1.placePentomino(Y, 3, 5);
+        game1.rotatePentominoAntiClkWise(Y);
+        game1.mirrorPentominoH(Y);
+
+        game1.display();
+
+        let X2 = new Pentomino('X');
+        let T2 = new Pentomino('T');
+        let L2 = new Pentomino('L');
+        let U2 = new Pentomino('U');
+        let N2 = new Pentomino('N');
+        let F2 = new Pentomino('F');
+        let I2 = new Pentomino('I');
+        let P2 = new Pentomino('P');
+        let Z2 = new Pentomino('Z');
+        let V2 = new Pentomino('V');
+        let W2 = new Pentomino('W');
+        let Y2 = new Pentomino('Y');
+
+        let game2 = new Game(new Board([0, 0], [5, 7]));
+
+        game2.placePentomino(L2, 3, 0);
+
+        game2.placePentomino(I2, 0, 4);
+        game2.rotatePentominoClkWise(I2);
+
+        game2.placePentomino(F2, 1, 1);
+        game2.mirrorPentominoV(F2);
+
+        game2.placePentomino(P2, 1, 4);
+        game2.rotatePentominoAntiClkWise(P2);
+
+        game2.placePentomino(X2, 3, 2);
+
+        game2.placePentomino(U2, 2, 6);
+        game2.rotatePentominoAntiClkWise(U2);
+
+        game2.placePentomino(Y2, 3, 4);
+        game2.mirrorPentominoV(Y2);
+
+        game2.display();
+
+        solutions.push(game1);
+        solutions.push(game2);
+        return solutions;
+    }
+}
+
+if(typeof module != 'undefined') {
+    module.exports = HintAI;
+}
---
+++ resources/tests/hint-ai/hint-ai-get-hint-test.js
@@ -0,0 +1,6 @@
+let assert = require('chai').assert;
+
+let HintAI = require('../../js/hint-ai.js');
+
+let hintAI = new HintAI();
+hintAI.getMockSolutions();
