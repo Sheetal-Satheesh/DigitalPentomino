da0c5f25db1184f5703e4da80ee1ab9f9948099f
krupa08 <krupa08@mail.uni-paderborn.de>
2021-02-05 23:02:00 +0530

    reduced collision offset

--- resources/js/visual.js
+++ resources/js/visual.js
@@ -176,9 +176,8 @@ class Visual {
             let left = undefined;
             let top = undefined;
             if(overlapp){
-                left = UIProperty.FunctionWidth + width * (positionX - 2)+ (width/2);
-                top = UIProperty.TrayHeight + width * (positionY - 2)-(width/2);
-           
+                left = UIProperty.FunctionWidth + width * (positionX - 2.4)+ (width/2);
+                top = UIProperty.TrayHeight + width * (positionY - 2.4)-(width/2);
 
                 if(htmlElement.style.zIndex >= 2000 ){
                     htmlElement.style.zIndex =1500;
