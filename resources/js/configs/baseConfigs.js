579d20588f19f42692ec393ea0f947ccb7787ec2
Ashwini Chachidanandan <ashwini2@campus.uni-paderborn.de>
2021-03-13 17:49:48 +0000

    Update baseConfigs.js

--- resources/js/configs/baseConfigs.js
+++ resources/js/configs/baseConfigs.js
@@ -4,6 +4,6 @@
 baseConfigs = {
     gameWidth: 25,
     gameHeight: 12,
-    boards: ["board_3x21a", "board_3x21b", "board_3x21c", "board_3x21d", "board_6x10", "board_8x8a", "board_4x15"],
+    boards: ["board_3x21a", "board_3x21b", "board_3x21c", "board_3x21d", "board_6x10", "board_8x8a", "board_4x15","board_12x20"],
     boardShape : "Block"
 };
