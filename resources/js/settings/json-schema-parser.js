ada5ecc3c1bb17bb2114301975ee4b3817d8e693
JanNB97 <j.bingemann@gmx.de>
2021-03-22 16:46:09 +0100

    Settings: Introduced json-schema-parser

---
+++ resources/js/settings/json-schema-parser.js
@@ -0,0 +1,13 @@
+class JsonSchemaParser {
+
+    /**
+     * Takes as input the settings schema and an actual instance and returns a seed
+     * @param schema
+     * @param values
+     */
+    static parseSettingsToSeed(schema, values) {
+        // TODO
+
+        return "no seed";
+    }
+}
