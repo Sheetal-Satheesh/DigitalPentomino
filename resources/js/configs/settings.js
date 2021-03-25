9fa11edf7014dcda52d29a13e3f21212df1097c1
mmenne1 <marcel.menne@gmx.de>
2021-03-26 00:04:31 +0100

    extended jsonform, corrected some ui bugs regarding settings window

--- resources/js/configs/settings.js
+++ resources/js/configs/settings.js
@@ -16,6 +16,7 @@ settings = {
 };
 
 
+
 // Example seeds:
 //
 // e.g. "partial hinting with skillTeaching enabled, indicating the Destination Posiitons and pentominos + 
@@ -65,31 +66,65 @@ const SettingsEntryType = {
  *  - Numbers must contain an entry 'decimals', which specifies the number of decimals
  */
 
-let language = "de";
-
-title = {
-    "en": "Hinting Strategy",
-    "de": "Hinting Strategie"
-}
-
-strategies = {
-    "en": ["full","partial","another"],
-    "de": ["vollständig","teilweise","andere"]
-}
 
 settingsSchema = {
-    hintingStrategy: {
-        "title": title[language],
-        "description": "Please pick a hinting strategy",
-        "type": "string",
-        "enum": strategies[language]
-      },
-    skillTeaching: {
-        type: 'boolean',
-        title: 'Skill Teaching',
-        decimals: 2
+    hinting: {
+        "type": "object",
+        "title": "Hinting",
+        "properties": {
+            hintingStrategy: {
+                "type": "string",
+                "title": "hintingStrategy",
+                "enum": ["full","partial"]
+            },
+            skillTeaching: {
+                "type": "boolean",
+                "title": "Skill Teaching hints?",       
+            },
+            indicateDestination: {
+                "type": "boolean",
+                "title": "Indication of Destination positions?",       
+            },
+            indicateDestination: {
+                "type": "boolean",
+                "title": "Indication of pentomino?",       
+            }
+        }
+    },
+    prefilling: {
+        "type": "object",
+        "title": "Prefilling",
+        "properties": {
+            prefillingStrategy: {
+                "type": "string",
+                "title": "prefillingStrategy",
+                "enum": ["distance"]
+            },
+            distanceValue: [
+                {
+                  "type": "range",
+                  "step": 1,
+                  "type": "integer",
+                  "title": "How much distance between pieces?",
+                  "default": 2,
+                  "minimum": 1,
+                  "exclusiveMinimum": true,
+                  "maximum": 10
+                }
+              ]
+        }
     }
 };
+
+
+//Multilanguage via variable:
+//let language = "de";
+//
+//title = {
+//    "en": "Hinting Strategy",
+//    "de": "Hinting Strategie"
+//}
+//title[language],
 
 // --- --- --- Parser --- --- ---
 // Input: seed and settings-schema-object
@@ -104,3 +139,7 @@ settingsSchema = {
 // Maybe help from https://github.com/jsonform/jsonform
 
 // JSON-schema: http://json-schema.org/
+
+
+
+
