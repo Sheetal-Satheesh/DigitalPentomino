973c743b0ecdb83e56b7965a84a6f284ead87336
JanNB97 <j.bingemann@gmx.de>
2021-03-29 17:14:52 +0200

    Add SettingsSchema singleton

--- resources/js/settings/settings-schema.js
+++ resources/js/settings/settings-schema.js
@@ -5,51 +5,98 @@
  *  - Numbers must contain an entry 'decimals', which specifies the number of decimals
  */
 
-const settingsSchema = {
-    hinting: {
-        "type": "object",
-        "title": "Hinting",
-        "properties": {
-            hintingStrategy: {
-                "type": "string",
-                "title": "hintingStrategy",
-                "enum": ["full","partial"]
-            },
-            skillTeaching: {
-                "type": "boolean",
-                "title": "Skill Teaching hints?",
-            },
-            indicateDestinationPosition: {
-                "type": "boolean",
-                "title": "Indication of Destination positions?",
-            },
-            indicateDestination: {
-                "type": "boolean",
-                "title": "Indication of pentomino?",
+const SettingsSchemaSingleton = (function () {
+    let instance;
+
+    function createInstance() {
+        return new SettingsSchema();
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
             }
+            return instance;
         }
-    },
-    prefilling: {
-        "type": "object",
-        "title": "Prefilling",
-        "properties": {
-            prefillingStrategy: {
-                "type": "string",
-                "title": "prefillingStrategy",
-                "enum": ["distance"]
-            },
-            distanceValue: {
-                "step": 1,
-                "type": "integer",
-                "title": "How much distance between pieces?",
-                "default": 2,
-                "minimum": 1,
-                "exclusiveMinimum": true,
-                "maximum": 10
-            }
+    };
+})();
+
+class SettingsSchema {
+    constructor() {
+        this._language = "en";
+        this._dirty = true;
+    }
+
+    setLanguage(language) {
+        if (!(this._language === language)) {
+            this._dirty = true;
+        }
+
+        this._language = language;
+    }
+
+    getLanguage() {
+        return this._language;
+    }
+
+    getSettingsSchema() {
+        if (this._dirty === false) {
+            return this._schema;
+        } else {
+            this._dirty = false;
+            return this.createSchema();
         }
     }
-};
+
+    createSchema() {
+        return this._schema = {
+            hinting: {
+                "type": "object",
+                "title": "Hinting",
+                "properties": {
+                    hintingStrategy: {
+                        "type": "string",
+                        "title": "hintingStrategy",
+                        "enum": ["full","partial"]
+                    },
+                    skillTeaching: {
+                        "type": "boolean",
+                        "title": "Skill Teaching hints?",
+                    },
+                    indicateDestinationPosition: {
+                        "type": "boolean",
+                        "title": "Indication of Destination positions?",
+                    },
+                    indicateDestination: {
+                        "type": "boolean",
+                        "title": "Indication of pentomino?",
+                    }
+                }
+            },
+            prefilling: {
+                "type": "object",
+                "title": "Prefilling",
+                "properties": {
+                    prefillingStrategy: {
+                        "type": "string",
+                        "title": "prefillingStrategy",
+                        "enum": ["distance"]
+                    },
+                    distanceValue: {
+                        "step": 1,
+                        "type": "integer",
+                        "title": "How much distance between pieces?",
+                        "default": 2,
+                        "minimum": 1,
+                        "exclusiveMinimum": true,
+                        "maximum": 10
+                    }
+                }
+            }
+        };
+    }
+}
 
 
 //Multilanguage via variable:
