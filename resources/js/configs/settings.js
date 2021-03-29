4eef9e08ff8890d2cf7a303b63046cdd197c5b18
JanNB97 <j.bingemann@gmx.de>
2021-03-29 16:58:33 +0200

    Introduce Settings singleton

--- resources/js/configs/settings.js
+++ resources/js/configs/settings.js
@@ -1,63 +1,37 @@
-settings = {
-    hinting: {
-        supportedHintingStrategies:["full","partial"],
-        hintingStrategy:"partial", // f=full (full hints are given), p=partial (only partial hints are given)
-        skillTeaching:"true", // 0=false (non-solvable positions are not indicated), 1=true (non-solvable situations are indicated on the board)
-        indicateDestination:"true", // 0=false (destinations are not indicated on the board), 1=true (destinations are indicated on the board)
-        indicatePentomino:"true" // 0=false (hint-related pentomino not highlighted on the board), 1=true (hint-related pentomino is highlighted on the board)
-    },
-    prefilling: {
-        supportedPrefillingStrategies:["distance"],
-        prefillingStrategy: "distance", // d=distance
-        distanceValue: "2", // <number>: min. distance between two pieces according to prefilling strategy
-        pieceLimit: "99" // <number>: maximum amount of pieces to be placed on the board
+const SettingsSingleton = (function () {
+    let instance;
+
+    function createInstance() {
+        return new Settings();
     }
-    
-};
 
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
 
-
-// Example seeds:
-//
-// e.g. "partial hinting with skillTeaching enabled, indicating the Destination Posiitons and pentominos + 
-//       distance-based prefilling with a minimum distance of 2 and maximum 5 pieces on the board":
-// -->  Hp111Pd25 (seed for configuration)
-
-const SettingsEntryType = {
-    BOOL: 0,
-    INT: 1,
-    STRING: 2,
-    ENUM: 3
-};
-
-/*settingsSchema = {
-    hinting: {
-        supportedHintingStrategies: {
-            type: SettingsEntryType.ENUM,
-            allowedValues: ["full","partial","another"],
-            titleEn: "Supported Hinting Strategies",
-            titleGe: "Unterstütze Hilf-Strategien"
-        },
-        skillTeaching: {
-            type: SettingsEntryType.BOOL,
-            titleEn: "Skill teaching",
-            titleGe: "Fähigkeits-lehrender Unterricht"
-        }, // 0=false (non-solvable positions are not indicated), 1=true (non-solvable situations are indicated on the board)
-        indicateDestination: {
-            type: SettingsEntryType.BOOL,
-            titleEn: "Indicate Destionation",
-            titleGe: ""
-        }, // 0=false (destinations are not indicated on the board), 1=true (destinations are indicated on the board)
-        indicatePentomino: {
-            type: SettingsEntryType.BOOL,
-            titleEn: "Indicate Pentomino",
-            titleGe: ""
-        } // 0=false (hint-related pentomino not highlighted on the board), 1=true (hint-related pentomino is highlighted on the board)
-    },
-    prefilling: {
-        // ...
+class Settings {
+    setSeed(seed) {
+        this._seed = seed;
     }
-};*/
+
+    getSeed() {
+        return this._seed;
+    }
+
+    setSettings(settings) {
+        this._settings = settings;
+    }
+
+    getSettings() {
+        return this._settings;
+    }
+}
 
 /** RULES
  *  - entries of type string only with enum specified
@@ -66,8 +40,7 @@ const SettingsEntryType = {
  *  - Numbers must contain an entry 'decimals', which specifies the number of decimals
  */
 
-
-settingsSchema = {
+const settingsSchema = {
     hinting: {
         "type": "object",
         "title": "Hinting",
@@ -122,21 +95,4 @@ settingsSchema = {
 //    "de": "Hinting Strategie"
 //}
 //title[language],
-
-// --- --- --- Parser --- --- ---
-// Input: seed and settings-schema-object
-// Output: setting-object
-// Generates setting-object based on settings-schema-object and populates it with values in seed
-
-// --- --- --- UI --- --- ---
-// Input: settings-schema-object
-// Output: Settings-website
-// (Website outputs on demand seed)
-
-// Maybe help from https://github.com/jsonform/jsonform
-
-// JSON-schema: http://json-schema.org/
-
-
-
 
