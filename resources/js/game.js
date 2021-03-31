f2ab706dd677bf6849c294a66460840a46d33708
asu <asu@mail.uni-paderborn.de>
2021-04-01 00:15:31 +0200

    revert pentomino postions

--- resources/js/game.js
+++ resources/js/game.js
@@ -236,8 +236,8 @@ class Game {
 
     _fillUpTray(){
         var allPentominos=[
-                    'F','L','N','P','Y','T',
-                    'U','V','W','Z','I','Z'];
+            'F','I','L','N','P','T',
+            'U','V','W','X','Y','Z'];
         allPentominos.forEach(function(pentominoType){
             let pentomino = new Pentomino(pentominoType); 
             this._tray.push(pentomino);
