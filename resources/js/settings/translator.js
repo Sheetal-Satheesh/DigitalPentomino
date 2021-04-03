6a35944d62c17aaed5108aaa76bf17d5ae01a78f
mmenne1 <marcel.menne@gmx.de>
2021-04-03 11:22:37 +0200

    null handling

--- resources/js/settings/translator.js
+++ resources/js/settings/translator.js
@@ -4,10 +4,15 @@ class Translator{
         //simple replacement of headers to language
         switch (language) {
             case "de":
-                document.getElementById("header-game-settings").textContent = 'Spieleinstellungen';
+                document.getElementById("header-game-settings") ? document.getElementById("header-game-settings").textContent = 'Spieleinstellungen' : console.log("Translation element nicht gefunden.");
+                document.getElementById("settings-submit-button") ? document.getElementById("settings-submit-button").value = 'Bestätigen' : console.log("Translation element nicht gefunden.");
+                document.getElementById("settings-close-button") ? document.getElementById("settings-close-button").value = 'Abbrechen' : console.log("Translation element nicht gefunden.");
                 break;
             case "en":
-                document.getElementById("header-game-settings").textContent = 'Game settings';
+                document.getElementById("header-game-settings") ? document.getElementById("header-game-settings").textContent = 'Game settings' : console.log("Translation element not found.");
+                document.getElementById("settings-submit-button") ? document.getElementById("settings-submit-button").value = 'Confirm' : console.log("Translation element not found.");
+                document.getElementById("settings-close-button") ? document.getElementById("settings-close-button").value = 'Abort' : console.log("Translation element not found.");
+
                 break;
             default:
                 console.log("Error: Unexpected language given!");
