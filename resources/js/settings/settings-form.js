f00e7a79636dfa02b7de132a65dedc3ba7526eb4
JanNB97 <j.bingemann@gmx.de>
2021-05-02 14:26:02 +0200

    Settingsform: support boolean entries

--- resources/js/settings/settings-form.js
+++ resources/js/settings/settings-form.js
@@ -1,8 +1,86 @@
 class SettingsForm {
-    static generateForm(element, schema) {
+
+    // === === === GENERATE FORM === === ===
+    static generateForm(formElement, schema, onSubmit) {
+
+        for (let heading in schema) {
+            let subSettings = schema[heading].properties;
+            for (let key in subSettings) {
+                let settingsEntry = subSettings[key];
+                let settingsEntryType = settingsEntry.type;
+                switch (settingsEntryType) {
+                    case "boolean":
+                        SettingsForm.addBooleanEntry(formElement, settingsEntry);
+                        break;
+                    case "string":
+                        SettingsForm.addStringEntry(formElement, settingsEntry);
+                        break;
+                    case "integer":
+                        SettingsForm.addIntegerEntry(formElement, settingsEntry);
+                        break;
+                    case "number":
+                        SettingsForm.addNumberEntry(formElement, settingsEntry);
+                        break;
+                    default:
+                        throw new Error("Unknown type: " + settingsEntryType)
+                }
+            }
+        }
+
+        formElement.appendChild(SettingsForm.createSubmitButton());
+        formElement.onSubmit(function () {
+            let data = SettingsForm.collectDataFromForm();
+            onSubmit(data);
+        });
+    }
+
+    static collectDataFromForm() {
+        return {};
+    }
+
+    static addBooleanEntry() {
         // TODO
     }
 
+    static addStringEntry(formElement, settingsEntry) {
+        formElement.appendChild(SettingsForm.createStringFormElement(settingsEntry));
+    }
+
+    static addNumberEntry() {
+        // TODO
+    }
+
+    static addIntegerEntry() {
+        // TODO
+    }
+
+    static createBooleanFormElement(settingsEntry) {
+        // TODO
+    }
+
+    static createStringFormElement(settingsEntry) {
+        let selectElement = document.createElement("select");
+
+        let i = 0;
+        for (let enumElement in settingsEntry.enum) {
+            let optionElement = document.createElement("option");
+            optionElement.innerHTML = settingsEntry.enumText[i];
+            optionElement.value = enumElement;
+            selectElement.appendChild(optionElement);
+            i++;
+        }
+
+        return selectElement;
+    }
+
+    static createSubmitButton() {
+        let submitButton = document.createElement("button");
+        submitButton.setAttribute("type", "submit");
+        submitButton.innerHTML = "Submit";
+        return submitButton;
+    }
+
+    // === === === UPDATE FORM === === ===
     static updateForm(formElement, schema, settings) {
         // TODO
     }
