5fd408739ab422122a5715c0b46ed2f5bd3c011b
krupa08 <krupa08@mail.uni-paderborn.de>
2021-05-17 20:41:12 +0530

    indicate area

--- resources/js/visual.js
+++ resources/js/visual.js
@@ -770,18 +770,16 @@ class Visual {
                         }
                         break;
                     case "area":
-                        for (let i = 0; i < 25; i++) {
                             let areaPos = this.indicateAreaCells(hintinPen, hintCommand)[0];
-                            let b = this.gameController.game()._board.positionIsValid(areaPos[i][0], areaPos[i][1]);
-                            if (b) {
-                                let areaPos = this.indicateAreaCells(hintinPen, hintCommand)[0];
-                                fieldvalue = document.getElementById("field_" + areaPos[i][0] + "," + areaPos[i][1]);
-                                prevBackground[i] = fieldvalue.style.background;
-                                fieldvalue.style.background = pentominoColor;
-                                this.hideArea(areaPos, prevBackground, timeoutFrame);
-                            }
-                        }
-                        break;
+                            for (let i = 0; i < areaPos.length; i++) {
+                                    let areaPos = this.indicateAreaCells(hintinPen, hintCommand)[0];
+                                    fieldvalue = document.getElementById("field_" + areaPos[i][0] + "," + areaPos[i][1]);
+                                    prevBackground[i] = fieldvalue.style.background;
+                                    fieldvalue.style.background = pentominoColor;
+                                }
+                            
+                            this.hideArea(areaPos, prevBackground, timeoutFrame);
+                            break;
                     default:
                         console.error("Hinting strategy unknown!");
                 }
