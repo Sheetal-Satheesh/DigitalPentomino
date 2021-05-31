12dd30bd444b92cf9f4046601047838a562b6030
Saurabh Raut <sraut@mail.uni-paderborn.de>
2021-05-31 20:56:33 +0200

    Added themes in the game

--- resources/js/themeSwitcher.js
+++ resources/js/themeSwitcher.js
@@ -1,32 +1,32 @@
-console.log('Theme switcher initiated');
+// console.log('Theme switcher initiated');
 
-let themeSwitches = document.getElementsByClassName('theme-switches');
-let style = localStorage.getItem('style');
+// let themeSwitches = document.getElementsByClassName('theme-switches');
+// let style = localStorage.getItem('style');
 
 
-if (style == null){
-    setTheme('default');
-} 
-else{
-    setTheme(style);
-} 
+// if (style == null){
+//     setTheme('default');
+// } 
+// else{
+//     setTheme(style);
+// } 
 
-for(let i of themeSwitches){
-    i.addEventListener('click', function(){
-        let theme = this.dataset.theme;
-        console.log(theme);
-        setTheme(theme);
-    });
-}
+// for(let i of themeSwitches){
+//     i.addEventListener('click', function(){
+//         let theme = this.dataset.theme;
+//         console.log(theme);
+//         setTheme(theme);
+//     });
+// }
 
-function setTheme(theme){
-    if(theme == 'default'){
-        document.getElementById('switcher-id').href = 'resources/css/style.css';
-    }
-    else if(theme == 'light'){
-        document.getElementById('switcher-id').href = 'resources/css/day-theme.css';
-    }else if (theme == 'ice') {
-        document.getElementById('switcher-id').href = 'resources/css/ice-theme.css';
-    }
-    localStorage.setItem('style', theme);
-}
+// function setTheme(theme){
+//     if(theme == 'default'){
+//         document.getElementById('switcher-id').href = 'resources/css/style.css';
+//     }
+//     else if(theme == 'light'){
+//         document.getElementById('switcher-id').href = 'resources/css/day-theme.css';
+//     }else if (theme == 'ice') {
+//         document.getElementById('switcher-id').href = 'resources/css/ice-theme.css';
+//     }
+//     localStorage.setItem('style', theme);
+// }
