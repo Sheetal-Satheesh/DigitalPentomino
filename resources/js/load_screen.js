48697fc7879e08bff85a2efd3326e53b36a0a605
krupa08 <krupa08@mail.uni-paderborn.de>
2021-06-22 00:03:50 +0530

    minor

--- resources/js/load_screen.js
+++ resources/js/load_screen.js
@@ -1,6 +1,6 @@
  /* jQuery Pre loader
-  -----------------------------------------------*/
-  //reference: https://codepen.io/hariharakumar/pen/VmOdBx
-$(window).load(function(){
-    $('.preloader').fadeOut(100000); // set duration in brackets    
-});
+               -----------------------------------------------*/
+ //reference: https://codepen.io/hariharakumar/pen/VmOdBx
+ $(window).load(function() {
+     $('.preloader').fadeOut(1000); // set duration in brackets    
+ });
