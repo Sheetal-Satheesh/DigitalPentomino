b15057d5fdfd69e2e6ab2639a0ba185ee726cead
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-03 23:13:49 +0530

    config file

---
+++ resources/js/configs/licensesConfig.js
@@ -0,0 +1,40 @@
+
+
+licensesConfig = {
+    // "backgroundImg1": {
+    //     "name": strings.licenses.backgroundImg,
+    //     "specifiedName": "",
+    //     "link": "",
+    //     "license": ""
+    // },
+    "solvedScreenMagician": {
+        "name": strings.licenses.solvedScreenMagicianImg,
+        "specifiedName": "wizard-free",
+        "link": "https://www.freepngimg.com/png/19721-wizard-free-png-image",
+        "license": "https://creativecommons.org/licenses/"
+    },
+    "solvedScreenBoy": {
+        "name": strings.licenses.solvedScreenBoy,
+        "specifiedName": "jboy-2",
+        "link": "https://www.deviantart.com/edicionesnaza/art/Boy-and-Girl-Icon-gif-By-alfreditha-394021423",
+        "license": "https://creativecommons.org/licenses/"
+    },
+    "solvedScreenGift": {
+        "name": strings.licenses.solvedScreenGift,
+        "specifiedName": "present",
+        "link": "https://www.freeiconspng.com/img/39913 ",
+        "license": "https://creativecommons.org/licenses/"
+    },
+    "backgroundMusic": {
+        "name": strings.licenses.backgroundMusic,
+        "specifiedName": "'Chee Zee Beach",
+        "link": "https://incompetech.filmmusic.io/song/3490-chee-zee-beach",
+        "license": "https://filmmusic.io/standard-license"
+    },
+    "functionsMusic": {
+        "name": strings.licenses.functionsMusic,
+        "specifiedName": "Wood",
+        "link": "https://www.zapsplat.com/sound-effect-category/wood/",
+        "license": "https://www.zapsplat.com/license-type/standard-license/"
+    }
+}
