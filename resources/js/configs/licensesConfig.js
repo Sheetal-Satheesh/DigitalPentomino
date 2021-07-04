6b88e3d8b178c2a57158cb2abef31e200f1320d3
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-04 18:46:42 +0530

    accessing lic-config file in index.html

--- resources/js/configs/licensesConfig.js
+++ resources/js/configs/licensesConfig.js
@@ -1,38 +1,41 @@
-
+if (typeof require != 'undefined') {
+    Pentomino = require('./pentomino.js');
+    Config = require('./multi-lang.js');
+}
 
 licensesConfig = {
     // "backgroundImg1": {
-    //     "name": strings.licenses.backgroundImg,
+    //     "name": strings.license.backgroundImg[lang],
     //     "specifiedName": "",
     //     "link": "",
     //     "license": ""
     // },
     "solvedScreenMagician": {
-        "name": strings.licenses.solvedScreenMagicianImg,
+        //"name": strings.settings.license.solvedScreenMagician[lang],
         "specifiedName": "wizard-free",
         "link": "https://www.freepngimg.com/png/19721-wizard-free-png-image",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenBoy": {
-        "name": strings.licenses.solvedScreenBoy,
+      //  "name": strings.license.solvedScreenBoy[lang],
         "specifiedName": "jboy-2",
         "link": "https://www.deviantart.com/edicionesnaza/art/Boy-and-Girl-Icon-gif-By-alfreditha-394021423",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenGift": {
-        "name": strings.licenses.solvedScreenGift,
+        //"name": strings.license.solvedScreenGift[lang],
         "specifiedName": "present",
         "link": "https://www.freeiconspng.com/img/39913 ",
         "license": "https://creativecommons.org/licenses/"
     },
     "backgroundMusic": {
-        "name": strings.licenses.backgroundMusic,
+        //"name": strings.license.backgroundMusic[lang],
         "specifiedName": "'Chee Zee Beach",
         "link": "https://incompetech.filmmusic.io/song/3490-chee-zee-beach",
         "license": "https://filmmusic.io/standard-license"
     },
     "functionsMusic": {
-        "name": strings.licenses.functionsMusic,
+        //"name": strings.license.functionsMusic[lang],
         "specifiedName": "Wood",
         "link": "https://www.zapsplat.com/sound-effect-category/wood/",
         "license": "https://www.zapsplat.com/license-type/standard-license/"
