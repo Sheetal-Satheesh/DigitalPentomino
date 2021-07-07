733313dca16b7b65afdb5ef7970795ea2b99844b
Jan Niklas Bingemann <jnb@uni-paderborn.de>
2021-07-07 17:53:42 +0200

    begin to create start pos html element

--- resources/js/settings/custom-settings-entry/start-pos-settings-entry.js
+++ resources/js/settings/custom-settings-entry/start-pos-settings-entry.js
@@ -4,7 +4,34 @@ class StartPosSettingsEntry extends CustomSettingsEntry {
     }
 
     create(settingsEntry) {
-        return document.createTextNode("TODO - implement me");
+        let div = document.createElement("div");
+        div.id = this._name;
+        div.name = this._name;
+
+        let saveButton = SettingsForm.createButton("Use current positions", {});
+        saveButton.onclick = (event) => this.handleClickedOnSave(event, div);
+        div.appendChild(saveButton);
+        div.appendChild(document.createElement("br"));
+        let clearButton = SettingsForm.createButton("Clear", {});
+        clearButton.onclick = (event) => this.handleClickedOnClear(event, div);
+        div.appendChild(clearButton);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(SettingsForm.createLabel("Start positions: "));
+        let resultLabel = SettingsForm.createLabel("None saved");
+        resultLabel.id = "startPiecePosLabel";
+        resultLabel.name = "startPiecePosLabel";
+        div.appendChild(resultLabel);
+        div.appendChild(document.createElement("br"));
+
+        return div;
+    }
+
+    handleClickedOnSave(event, div) {
+        console.log("save");
+    }
+
+    handleClickedOnClear(event, div) {
+        console.log("clear");
     }
 
     collect(formElement) {
