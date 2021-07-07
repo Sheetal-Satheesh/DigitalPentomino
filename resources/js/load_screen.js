676e2863a6a438edfdd152ca8750de7657d83949
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-08 00:30:08 +0530

    tuca loading fixed with  data load time with new png

--- resources/js/load_screen.js
+++ resources/js/load_screen.js
@@ -1,6 +1,8 @@
  /* jQuery Pre loader
                -----------------------------------------------*/
  //reference: https://codepen.io/hariharakumar/pen/VmOdBx
+ const perfData = window.performance.timing;
+const pageLoadTime = perfData.loadEventEnd - perfData.navigationStart;
  $(window).load(function() {
-     $('.preloader').fadeOut(200000000000); // set duration in brackets    
+     $('.preloader').fadeOut(pageLoadTime); // set duration in brackets    
  });
