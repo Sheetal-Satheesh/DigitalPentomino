67663fd494e88f70b62eaecdff4f5fde9683dedc
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-10 23:17:13 +0530

    all bugs fixed

--- resources/js/configs/licensesConfig.js
+++ resources/js/configs/licensesConfig.js
@@ -1,4 +1,4 @@
-
+let lang = SettingsSingleton.getInstance().getSettings().general.language;
 
 licensesConfig = {
     // "backgroundImg1": {
@@ -8,31 +8,31 @@ licensesConfig = {
     //     "license": ""
     // },
     "solvedScreenMagician": {
-        //"name": strings.settings.license.solvedScreenMagician,
+        "name": strings.settings.license.solvedScreenMagician[lang],
         "specifiedName": "wizard-free",
         "link": "https://www.freepngimg.com/png/19721-wizard-free-png-image",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenBoy": {
-      //  "name": strings.settings.license.solvedScreenBoy,
+        "name": strings.settings.license.solvedScreenBoy[lang],
         "specifiedName": "jboy-2",
         "link": "https://www.deviantart.com/edicionesnaza/art/Boy-and-Girl-Icon-gif-By-alfreditha-394021423",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenGift": {
-      //  "name": strings.settings.license.solvedScreenGift,
+        "name": strings.settings.license.solvedScreenGift[lang],
         "specifiedName": "present",
         "link": "https://www.freeiconspng.com/img/39913 ",
         "license": "https://creativecommons.org/licenses/"
     },
     "backgroundMusic": {
-        //"name": strings.settings.license.backgroundMusic,
+        "name": strings.settings.license.backgroundMusic[lang],
         "specifiedName": "'Chee Zee Beach",
         "link": "https://incompetech.filmmusic.io/song/3490-chee-zee-beach",
         "license": "https://filmmusic.io/standard-license"
     },
     "functionsMusic": {
-        //"name": strings.settings.license.functionsMusic,
+        "name": strings.settings.license.functionsMusic[lang],
         "specifiedName": "Wood",
         "link": "https://www.zapsplat.com/sound-effect-category/wood/",
         "license": "https://www.zapsplat.com/license-type/standard-license/"
