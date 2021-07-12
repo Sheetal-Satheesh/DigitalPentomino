d2391388b683d58973d6f2c4efd6773a221acfa6
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-13 00:14:36 +0530

    commenting unwanted code

--- resources/js/configs/licensesConfig.js
+++ resources/js/configs/licensesConfig.js
@@ -1,5 +1,4 @@
 let lang = SettingsSingleton.getInstance().getSettings().general.language;
-
 licensesConfig = {
     // "backgroundImg1": {
     //     "name": strings.license.backgroundImg[lang],
@@ -9,31 +8,31 @@ licensesConfig = {
     // },
     "solvedScreenMagician": {
         "name": strings.settings.license.solvedScreenMagician[lang],
-        "specifiedName": "wizard-free",
+        //"specifiedName": "wizard-free",
         "link": "https://www.freepngimg.com/png/19721-wizard-free-png-image",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenBoy": {
         "name": strings.settings.license.solvedScreenBoy[lang],
-        "specifiedName": "jboy-2",
+        //"specifiedName": "jboy-2",
         "link": "https://www.deviantart.com/edicionesnaza/art/Boy-and-Girl-Icon-gif-By-alfreditha-394021423",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenGift": {
         "name": strings.settings.license.solvedScreenGift[lang],
-        "specifiedName": "present",
+        //"specifiedName": "present",
         "link": "https://www.freeiconspng.com/img/39913 ",
         "license": "https://creativecommons.org/licenses/"
     },
     "backgroundMusic": {
         "name": strings.settings.license.backgroundMusic[lang],
-        "specifiedName": "'Chee Zee Beach",
+        //"specifiedName": "'Chee Zee Beach",
         "link": "https://incompetech.filmmusic.io/song/3490-chee-zee-beach",
         "license": "https://filmmusic.io/standard-license"
     },
     "functionsMusic": {
         "name": strings.settings.license.functionsMusic[lang],
-        "specifiedName": "Wood",
+        //"specifiedName": "Wood",
         "link": "https://www.zapsplat.com/sound-effect-category/wood/",
         "license": "https://www.zapsplat.com/license-type/standard-license/"
     }
