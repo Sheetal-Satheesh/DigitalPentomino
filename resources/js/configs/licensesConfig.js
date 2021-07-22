6ffb8c25ef815810a5c7ae73d935816cd2eca991
krupa08 <krupa08@mail.uni-paderborn.de>
2021-07-23 03:55:49 +0530

    loading images dynamically into the licences table

--- resources/js/configs/licensesConfig.js
+++ resources/js/configs/licensesConfig.js
@@ -1,41 +1,34 @@
-//creating licences requires adding details in this file
-//after adding details here, add images in handleclickedlicences()
-// var url = document.location.href;
-// var newURL = window.location.pathname;
-// 	newURL = url.substring(url.lastIndexOf("/") + 1, url.length);
-// let lang = SettingsSingleton.getInstance().getSettings().general.language;
+//Instructions to add licences : creating licences requires adding details in this file
+//after adding details here, add images in the licences folder, keep the name of the images similar to the headings in this file
+//because the images get dynamically loaded into the licences table.
+//Also add lanuage translations in the license section of strings.js file
 licensesConfig = {
     "solvedScreenMagician": {
         "name": strings.settings.license.solvedScreenMagician,
-        //"image": " ",
         "Author": "Alexis Bailey",
         "link": "https://www.freepngimg.com/png/19721-wizard-free-png-image",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenBoy": {
         "name": strings.settings.license.solvedScreenBoy,
-        //"image": " ",
         "Author": "Edicionesnaza",
         "link": "https://www.deviantart.com/edicionesnaza/art/Boy-and-Girl-Icon-gif-By-alfreditha-394021423",
         "license": "https://creativecommons.org/licenses/"
     },
     "solvedScreenGift": {
         "name": strings.settings.license.solvedScreenGift,
-        //"image": " ",
         "Author": "Ahkâm",
         "link": "https://www.freeiconspng.com/img/39913 ",
         "license": "https://creativecommons.org/licenses/"
     },
     "backgroundMusic": {
         "name": strings.settings.license.backgroundMusic,
-        //"image": " ",
         "Author": "Kevin MacLeod",
         "link": "https://incompetech.filmmusic.io/song/3490-chee-zee-beach",
         "license": "https://filmmusic.io/standard-license"
     },
     "functionsMusic": {
         "name": strings.settings.license.functionsMusic,
-        //"image": " ",
         "Author": "ZapSplat",
         "link": "https://www.zapsplat.com/sound-effect-category/wood/",
         "license": "https://www.zapsplat.com/license-type/standard-license/"
