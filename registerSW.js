8e3135711a1fd533e90e8656bf88eea34a6600a8
mmenne1 <marcel.menne@gmx.de>
2021-08-02 23:33:58 +0200

    Fixed problem with arrows

--- registerSW.js
+++ registerSW.js
@@ -1,55 +1,11 @@
-//general functionality of service worker
-if ("serviceWorker" in navigator){
-    //Register the service worker
-    navigator.serviceWorker.register("sw.js").then(registration => {
-
-        console.log("1: Registration of SW.");
-
-        //Check if new service worker is aquired
-        registration.addEventListener('updatefound', ev => {
-            
-            //Save new service Worker from state registration.installing
-            installingWorker = registration.installing;
-            console.log("2: Installing Worker state: " + installingWorker.state);
-            console.log(installingWorker.state);
-
-            installingWorker.onstatechange = function (){
-                switch (installingWorker.state) {
-                
-                    //Check if new service worker is installed
-                    case 'installed':
-
-                      console.log("3: Installing Worker state: " + installingWorker.state);
-                      if (navigator.serviceWorker.controller) {
-                        // new updated service Worker available
-                        /*TBD: console.log("New updated service Worker available.");
-                        let notification = document.getElementById('notification');
-                        notification.className = 'show';
-                        console.log("3: Show notification bar...");*/
-                      }
-                      // No update available
-                      break;
-                  }
-            }
-
-        });
-
-        console.log("SW registered!");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW Registration failed!");
-        console.log(error);
-    })
-
-    //Refreshing
-    let refreshing;
-    navigator.serviceWorker.addEventListener('controllerchange', function () {
-      if (refreshing) return;
-      refreshing = true;
-    });
-
-
-
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
 } else {
-    console.log("Nothing found.");
+  console.log('CLIENT: service worker is not supported.');
 }
