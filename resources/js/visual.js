44e6bd063ad7f21da35bd339c7ca9539a7dcc835
krupa08 <krupa08@mail.uni-paderborn.de>
2021-08-16 06:38:16 +0530

    removal of unwanted code

--- resources/js/visual.js
+++ resources/js/visual.js
@@ -110,7 +110,6 @@ class Visual {
             audio.play();
         }
         this.positionPiece(pentomino);
-        console.log(this.listAllEventListeners());
         if (cmdProperty.cmdType != CommandTypes.Shadow) {
             this.checkIfGameWon();
             if (this.gameController.getOperationCount() == 1) {
@@ -118,43 +117,6 @@ class Visual {
             }
         }
     }
-    listAllEventListeners() {
-  const allElements = Array.prototype.slice.call(document.querySelectorAll('*'));
-  allElements.push(document);
-  allElements.push(window);
-
-
-
-  const types = [];
-
-
-
-  for (let ev in window) {
-    if (/^on/.test(ev)) types[types.length] = ev;
-  }
-
-
-
-  let elements = [];
-  for (let i = 0; i < allElements.length; i++) {
-    const currentElement = allElements[i];
-    for (let j = 0; j < types.length; j++) {
-      if (typeof currentElement[types[j]] === 'function') {
-        elements.push({
-          "node": currentElement,
-          "type": types[j],
-          "func": currentElement[types[j]].toString(),
-        });
-      }
-    }
-  }
-
-
-
-  return elements.sort(function(a,b) {
-    return a.type.localeCompare(b.type);
-  });
-}
 
     checkIfGameWon() {
         if (this.gameController.game()._board.isSolved()) {
