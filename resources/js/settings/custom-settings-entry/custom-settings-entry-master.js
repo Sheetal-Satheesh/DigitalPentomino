9bc91b1e895ebb5e27c8eb7e5e5486246d9dbb9e
Jan Niklas Bingemann <jnb@uni-paderborn.de>
2021-09-08 18:14:44 +0200

    docu

--- resources/js/settings/custom-settings-entry/custom-settings-entry-master.js
+++ resources/js/settings/custom-settings-entry/custom-settings-entry-master.js
@@ -1,3 +1,8 @@
+/**
+ * Singleton, which can be used to access a class of type {@link CustomSettingsEntryMaster}, which manages all
+ * registered custom settings entries.
+ * @type {{getInstance: (function(): CustomSettingsEntryMaster)}}
+ */
 const CustomSettingsEntrySingleton = (function () {
     let instance;
 
@@ -16,7 +21,7 @@ const CustomSettingsEntrySingleton = (function () {
 })();
 
 /**
- * A singleton which manages all custom setting entries. An entries can be added by registering a handlers in the
+ * A class which manages all custom setting entries. An entries can be added by registering a handlers in the
  * constructor. A handler is implemented by inheriting from the superclass {@link CustomSettingsEntry}.
  *
  * The singleton get be used anywhere to {@link #get} an entry by its respective name.
