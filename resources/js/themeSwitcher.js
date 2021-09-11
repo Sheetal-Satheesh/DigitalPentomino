eefc03aac4e3afbb36704520f501454be7208353
Saurabh Raut <sraut@mail.uni-paderborn.de>
2021-09-11 11:57:45 +0200

    Removed redundant console.logs

--- resources/js/themeSwitcher.js
+++ resources/js/themeSwitcher.js
@@ -1,9 +1,7 @@
 //consider 4 ids:
 // ids: defaultTheme, dayTheme, iceTheme, blackAndWhiteTheme
 document.getElementById('switcher-id').href = 'resources/css/themes/defaultTheme.css';
-console.log('Theme switcher initiated');
 let style = localStorage.getItem('style');
-console.log('style--->', style);
 
 if (style == null){
      setTheme('default');
