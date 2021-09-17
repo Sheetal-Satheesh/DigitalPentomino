e150c9bc3b4f90642671d27146baa85307fd4cd5
Saurabh Raut <sraut@mail.uni-paderborn.de>
2021-09-17 15:42:24 +0200

    Minor change

--- resources/js/pentomino.js
+++ resources/js/pentomino.js
@@ -83,7 +83,7 @@ class Pentomino {
 
     setPentominoColors(colorNumber) {
         let themeStyle = SettingsSingleton.getInstance().getSettings().theming.theme;
-        var colorsArrayDefault = ['#cc2828', '#cccc28', '#7acc28', '#28cc28', '#cc28cc', '#28cc7a', '#28cccc', '#287acc', '#2828cc', '#cc287a', '#cc7a28', '#7a28cc'];
+        let colorsArrayDefault = ['#cc2828', '#cccc28', '#7acc28', '#28cc28', '#cc28cc', '#28cc7a', '#28cccc', '#287acc', '#2828cc', '#cc287a', '#cc7a28', '#7a28cc'];
         //Dynamic configuring of colors
         if (themeStyle == 'blackAndWhiteTheme') {
             return '#3e3d3d';
