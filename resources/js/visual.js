4763ff1c98572a7e9fe23200f700d7e33090817d
Sheetal Satheesh <sheetal@campus.uni-paderborn.de>
2021-09-25 23:04:43 +0200

    Changed Button Orientation

--- resources/js/visual.js
+++ resources/js/visual.js
@@ -455,10 +455,10 @@ class Visual {
                 document.getElementById('pieceManipulation').style.top = 'calc(' + y2Position + 'px - ' + (width * 2) + 'vw)';
                 document.getElementById('pieceManipulation').style.display = 'block';
                 document.documentElement.style.setProperty("--rotateV", "-133deg");
-                document.documentElement.style.setProperty("--rotateH", "-28deg");
-                document.documentElement.style.setProperty("--buttonRotA", "28deg");
+                document.documentElement.style.setProperty("--rotateH", "-98deg");
+                document.documentElement.style.setProperty("--buttonRotC", "28deg");
                 document.documentElement.style.setProperty("--buttonRotB", "63deg");
-                document.documentElement.style.setProperty("--buttonRotC", "98deg");
+                document.documentElement.style.setProperty("--buttonRotA", "98deg");
                 document.documentElement.style.setProperty("--buttonRotD", "133deg");
             }
         } else if ((x > 0) && (x < 170)) {
@@ -467,22 +467,22 @@ class Visual {
             document.getElementById('pieceManipulation').style.top = 'calc(' + y2Position + 'px - ' + (width * 2) + 'vw)';
             document.getElementById('pieceManipulation').style.display = 'block';
             document.documentElement.style.setProperty("--rotateV", "168deg");
-            document.documentElement.style.setProperty("--rotateH", "48deg");
-            document.documentElement.style.setProperty("--buttonRotA", "-48deg");
+            document.documentElement.style.setProperty("--rotateH", "128deg");
+            document.documentElement.style.setProperty("--buttonRotC", "-48deg");
             document.documentElement.style.setProperty("--buttonRotB", "-88deg");
-            document.documentElement.style.setProperty("--buttonRotC", "-128deg");
+            document.documentElement.style.setProperty("--buttonRotA", "-128deg");
             document.documentElement.style.setProperty("--buttonRotD", "-168deg");
         }
         else {
             document.getElementById('pieceManipulation').style.left = 'calc(' + x1Position + 'px - ' + (width * 0.05) + 'vw)';
             document.getElementById('pieceManipulation').style.top = 'calc(' + y2Position + 'px - ' + (width * 2) + 'vw)';
             document.getElementById('pieceManipulation').style.display = 'block';
-            document.documentElement.style.setProperty("--rotateV", "-228deg");
-            document.documentElement.style.setProperty("--rotateH", "-108deg");
-            document.documentElement.style.setProperty("--buttonRotA", "108deg");
-            document.documentElement.style.setProperty("--buttonRotB", "148deg");
-            document.documentElement.style.setProperty("--buttonRotC", "188deg");
-            document.documentElement.style.setProperty("--buttonRotD", "228deg");
+            document.documentElement.style.setProperty("--rotateV", "-108deg");
+            document.documentElement.style.setProperty("--rotateH", "-148deg");
+            document.documentElement.style.setProperty("--buttonRotD", "108deg");
+            document.documentElement.style.setProperty("--buttonRotA", "148deg");
+            document.documentElement.style.setProperty("--buttonRotB", "188deg");
+            document.documentElement.style.setProperty("--buttonRotC", "228deg");
         }
 
     }
